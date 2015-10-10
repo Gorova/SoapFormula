fa58dbab835fd23d089bf5d64c75b84214d556f4
Gorova <gorova2006gmail.com>
2015-10-10 11:35:30 +0300

    web

--- SoapFormula.Web/Controllers/HomeController.cs
+++ SoapFormula.Web/Controllers/HomeController.cs
@@ -14,10 +14,76 @@ namespace SoapFormula.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private IKernel kernel;
+        private IRepository repo;
+
+        public HomeController()
+        {
+            this.kernel = new StandardKernel(new LibraryModule());
+            this.repo = kernel.Get<IRepository>();
+        }
         public ActionResult Index()
+        {
+
+            //repo.Add(new Category { Name = "ScrubSoap" });
+            //repo.Add(new Category { Name = "Baby's" });
+            //repo.Save();
+            var category = repo.Get<Category>();
+            return View(category);
+        }
+
+        public ActionResult Create()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            repo.Add(category);
+            repo.Save();
+
+            return Redirect("Index");
+        }
+
+        public ActionResult Delete()
+        {
+            var list = repo.Get<Category>();
+            ViewBag.Category = new SelectList(list, "Id", "Name", 2);
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(string category)
+        {
+            int a = Int32.Parse(category);
+            var comp = repo.Get<Category>().FirstOrDefault(i => i.Id == a);
+            repo.Delete(comp);
+            repo.Save();
+
+            return Redirect("Index");
+        }
+
+        public ActionResult Edit()
+        {
+            return ViewBag();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Category category)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Get(int id)
+        {
+            var a = repo.Get<Category>().FirstOrDefault(i => i.Id == id);
+            ViewBag.Inform = "Вы выбрали категорию" + a.Name;
+            return View("Get");
+        }
+        
 
         public ActionResult About()
         {
